3e9d62882b4d50de30220d497f1e83b750245a2b
Ignazio Margiotta <ignaziomargiotta@MacBook-Air-di-Ignazio.local>
2021-09-09 00:01:18 +0200

    Test ACF in Page

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,35 @@
 import Head from 'next/head'
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
+import { getHomeDetails } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+export default function Index({ AllDetails: { page } }) {
+  const Page = page
+  console.log(Page);
 
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.featuredImage?.node}
-              date={heroPost.date}
-              author={heroPost.author?.node}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {Page.home.titolo}
+          <br />
+          {Page.home.testo}
         </Container>
       </Layout>
     </>
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
+export async function getStaticProps({}) {
+  const AllDetails = await getHomeDetails('/home')
+
   return {
-    props: { allPosts, preview },
+    props: { AllDetails },
   }
 }
